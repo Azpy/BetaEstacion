2dca9c4a26b536f83d43cc7c0777813cb543f762
Jonthan <jpuente.guerrero@hotmail.com>
2018-04-20 00:10:19 -0500

    Cambios FrmRegistro

--- Estacionamiento/FrmRegistro.Designer.cs
+++ Estacionamiento/FrmRegistro.Designer.cs
@@ -295,6 +295,7 @@
             this.button5.TabIndex = 6;
             this.button5.Text = "Cancelar";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // dataGridView1
             // 
--- Estacionamiento/FrmRegistro.cs
+++ Estacionamiento/FrmRegistro.cs
@@ -17,7 +17,15 @@ namespace Estacionamiento
             InitializeComponent();
         }
 
-
+        void resettxt()
+        {
+            txtNombre.Text = "";
+            txtMatricula.Text = "";
+            txtModelo.Text = "";
+            txtPlaca.Text = "";
+            rdbInvitado.Checked = false;
+            cmbPreferencia.SelectedIndex = 0;
+        }
         BaseDeDatos bd = new BaseDeDatos();
         int val_inv, cons_id;
         string res_id, mat_busq,id_user,sel_ind, sel_inv;
@@ -77,16 +85,16 @@ namespace Estacionamiento
                 res_id = bd.selectstring(cons_id);
                 string auto = "insert into autos(modelo_auto,matricula_auto,idusuario) values('" + txtModelo.Text + "','" + txtPlaca.Text + "', " + res_id + ")";
                 if (bd.executecommand(auto)) { 
-                MessageBox.Show("Registro agregado correctamente");
+                    MessageBox.Show("Registro agregado correctamente");
+                    resettxt();
                 }
                 else
                 {
-                    MessageBox.Show("Error al agregar");
+                    MessageBox.Show("Error al agregar registro");
                 }
             }
-            FrmRegistro reg = new FrmRegistro();
-            reg.Show();
-            this.Hide();
+            resettxt();
+            dataGridView1.DataSource = bd.SelectDataTable("select mat_usuario,nombre, modelo_auto,matricula_auto from usuarios U, autos A  where U.idusuario = A.idusuario");
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -94,6 +102,16 @@ namespace Estacionamiento
             FrmControl control = new FrmControl();
             control.Show();
             this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseas salir de la aplicación?", "Estacionamiento de FIME",
+                  MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                  == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -110,49 +128,73 @@ namespace Estacionamiento
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            mat_busq = txtMatricula.Text;
-            txtMatricula.Text = bd.selectstring("select mat_usuario from usuarios where mat_usuario =" + mat_busq + "");
-            txtNombre.Text = bd.selectstring("select nombre from usuarios where mat_usuario =" + mat_busq + "");
-            sel_ind =  bd.selectstring("select preferencia from usuarios where mat_usuario =" + mat_busq + "");
-            cmbPreferencia.SelectedIndex = Convert.ToInt16(sel_ind);
-            id_user = bd.selectstring("select idusuario from usuarios where mat_usuario= " + mat_busq + "");
-            txtModelo.Text = bd.selectstring("select modelo_auto from autos where idusuario = " + id_user + "");
-            txtPlaca.Text = bd.selectstring("select matricula_auto from autos where idusuario = " + id_user + "");
+            if(txtMatricula.Text != "") { 
+                mat_busq = txtMatricula.Text;
+                txtMatricula.Text = bd.selectstring("select mat_usuario from usuarios where mat_usuario =" + mat_busq + "");
+                if (txtMatricula.Text == "")
+                {
+                    MessageBox.Show("No se encontraron coincidencias");
+                    resettxt();
+                }
+                else
+                {
+                    txtNombre.Text = bd.selectstring("select nombre from usuarios where mat_usuario =" + mat_busq + "");
+                    sel_ind = bd.selectstring("select preferencia from usuarios where mat_usuario =" + mat_busq + "");
+                    cmbPreferencia.SelectedIndex = Convert.ToInt16(sel_ind);
+                    id_user = bd.selectstring("select idusuario from usuarios where mat_usuario= " + mat_busq + "");
+                    txtModelo.Text = bd.selectstring("select modelo_auto from autos where idusuario = " + id_user + "");
+                    txtPlaca.Text = bd.selectstring("select matricula_auto from autos where idusuario = " + id_user + "");
 
-            sel_inv = bd.selectstring("select invitado from usuarios where mat_usuario =" + mat_busq + "");
-            if (sel_inv == "0")
-            {
-                val_invb = false;
-                rdbInvitado.Checked = val_invb;
+                    sel_inv = bd.selectstring("select invitado from usuarios where mat_usuario =" + mat_busq + "");
+                    if (sel_inv == "0")
+                    {
+                        val_invb = false;
+                        rdbInvitado.Checked = val_invb;
+                    }
+                    else
+                    {
+                        val_invb = true;
+                        rdbInvitado.Checked = val_invb;
+                    }
+                }
             }
             else
             {
-                val_invb = true;
-                rdbInvitado.Checked = val_invb;
+                MessageBox.Show("Favor de ingresar matricula");
             }
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
-            mat_busq = txtMatricula.Text;
-            string cons_id = "select idusuario from usuarios where mat_usuario =" + mat_busq + "";
-            res_id = bd.selectstring(cons_id);
-            string eliminar = "delete from usuarios where mat_usuario =" + mat_busq + "";
-            if (bd.executecommand(eliminar))
+            if (txtMatricula.Text != "")
             {
-                string elim_auto = "delete from usuarios where idusuario =" + res_id + "";
-                if (bd.executecommand(elim_auto))
+                if (MessageBox.Show("Deseas eliminar el registro con Matricula: " + txtMatricula.Text + "", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
-                    MessageBox.Show("Registro borrado correctamente");
-                }
-                else
-                {
-                    MessageBox.Show("Error al borrar registro");
+                    mat_busq = txtMatricula.Text;
+                    string cons_id = "select idusuario from usuarios where mat_usuario =" + mat_busq + "";
+                    res_id = bd.selectstring(cons_id);
+                    string eliminar = "delete from usuarios where mat_usuario =" + mat_busq + "";
+                    if (bd.executecommand(eliminar))
+                    {
+                        string elim_auto = "delete from autos where idusuario =" + res_id + "";
+                        if (bd.executecommand(elim_auto))
+                        {
+                            MessageBox.Show("Registro borrado correctamente");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error al borrar registro");
+                            resettxt();
+                        }
+                    }
                 }
             }
-            FrmRegistro reg = new FrmRegistro();
-            reg.Show();
-            this.Hide();
+            else
+            {
+                MessageBox.Show("Favor de ingresar matricula");
+            }
+            resettxt();
+            dataGridView1.DataSource = bd.SelectDataTable("select mat_usuario,nombre, modelo_auto,matricula_auto from usuarios U, autos A  where U.idusuario = A.idusuario");
         }
 
         private void btnModificar_Click(object sender, EventArgs e)
@@ -168,12 +210,20 @@ namespace Estacionamiento
                 if (bd.executecommand(modif_auto))
                 {
                     MessageBox.Show("Registro actualizado correctamente");
+                    txtNombre.Text = "";
+                    txtMatricula.Text = "";
+                    txtModelo.Text = "";
+                    txtPlaca.Text = "";
+                    rdbInvitado.Checked = false;
+                    cmbPreferencia.SelectedIndex = 0;
                 }
                 else
                 {
                     MessageBox.Show("Error al modificar registro");
+                    resettxt();
                 }
             }
+            dataGridView1.DataSource = bd.SelectDataTable("select mat_usuario,nombre, modelo_auto,matricula_auto from usuarios U, autos A  where U.idusuario = A.idusuario");
         }
     }
 }
